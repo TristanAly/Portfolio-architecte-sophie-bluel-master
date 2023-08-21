2785e01d8413bf04eea82354d3ca0f55be2f8094
TristanAly <tristan.aly@gmail.com>
2023-08-21 11:00:02 +0200

    Test delete Works

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/auth.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/auth.js
@@ -4,27 +4,12 @@ const password = document.querySelector("#password");
 const submit = document.querySelector("form");
 const errorSign = document.querySelector("#demo");
 
-let emailTest = "sophie.bluel@test.tld"
-let passwordTest = "S0phie"
-// CREDENTIALS
+
 submit.addEventListener('submit', e => {
-    e.preventDefault();
-    
-    AuthData();
-    autoRedirect();
-
-    if (email.value == "" || password.value == "") {
-        errorSign.innerHTML = `email or password is empty`
-      } else {
-        console.log(
-          `This form has a username of ${email.value} and password of ${password.value}`
-        );
-      }
-});
-
-function AuthData() {
+      e.preventDefault();
+    // CREDENTIALS
     let credentials = btoa(`${email.value}:${password.value}`);
-    // (B) FETCH WITH HTTP AUTH
+    // FETCH WITH HTTP AUTH
 fetch (Url + "users/login", {
       method: 'POST',
       headers: new Headers({ 
@@ -36,24 +21,18 @@ fetch (Url + "users/login", {
         "password": password.value
       })
     })
-    // // (C) SERVER RESPONSE
+    // SERVER RESPONSE
     .then(res => {
-      if (res.status != 200) { throw new Error("Bad Server Response"); }
-      console.log(res.token);
-      return res.json();
+      if (res.ok) { return res.json(); }
+      else { return Promise.reject(res.status);}
     })
-    .then(res => localStorage.setItem('token', res.token))
-   
-    // (D) HANDLE ERRORS (OPTIONAL)
-    .catch(err => console.error(err));
-}
-
-async function isLoggedIn () {
-    const token = localStorage.getItem('token')
-    if (!token) return false
-  }
-async function autoRedirect () {
-    const validLogin = await isLoggedIn()
-    if (!validLogin) window.location = 'index.html';
-    else window.location.href = 'index.html';
-  }
+    .then(res => {
+      localStorage.setItem('token', res.token);
+      window.location.href = 'index.html';
+  })
+    //  HANDLE ERRORS (OPTIONAL)
+    .catch(err => {
+      console.error(err)
+      errorSign.innerHTML = `email or password is incorrect`
+    });
+});
--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
@@ -2,7 +2,7 @@ const Url = "http://localhost:5678/api/";
 const all = document.querySelector(".all_button");
 const postsContainer = document.querySelector(".gallery");
 const filterContainer = document.querySelector(".filter");
-const editionGallery = document.querySelector(".edition_gallery");
+const editionGallery = document.querySelector(".edition_mode");
 
 let postsData = "";
 let filterData = "";
@@ -20,7 +20,7 @@ async function fetchWorks() {
   console.log(filterData);
   filterData.map((filter) => GenerateButtonFilter(filter));
 };
-
+editionModeActive()
 fetchWorks().catch(error => {
   error.message; // 'An error has occurred: 404'
 });
@@ -97,78 +97,139 @@ const handleFilterGallery = (param) => {
 const resetGallery = () => {
   postsContainer.innerHTML = "";
   postsData.map((post) => GenerateGallery(post));
-}
+};
 
 // ----------- EditionMode -------------
 
+function editionModeActive () {
+  const token = window.localStorage.getItem('token')
+  if (token) {
+    const bandeau = document.querySelector(".bandeau")
+    bandeau.style.display = "flex"
+    const editbutton = document.querySelectorAll("#active")
+    editbutton.forEach((userItem) => {
+      userItem.style.display = "inline-flex"
+    });
+  }
+};
 
-// const editionMode = (photosModal) => {
-// function genererPhotosModal(photosModal) {
-//   //Création d'une boucle qui va prendre toutes les photos
-//   for (let i = 0; i < photosModal.length; i++) {
-//     // Création des balises
-//     const article = photosModal[i];
+editionGallery.addEventListener('click', customGallery)
 
-//     const sectionGallery = document.querySelector(".galleryModal");
+const iconDelete = document.querySelector(".delete_button")
+iconDelete.addEventListener("click", (e) => {
+    console.log("DeleteClick");
+    e.preventDefault();
+});
 
-//     const articleElement = document.createElement("article");
-//     articleElement.classList.add("photosRealisation");
-//     articleElement.dataset.id = [i];
+function customGallery (){
+ const modale = document.querySelector(".modal")
+ modale.style.display = "block"
+fetchGallery().catch(error => {
+  error.message; // 'An error has occurred: 404'
+});
+console.log("test")
 
-//     const idElement = document.createElement("p");
-//     idElement.innerText = article.id;
+};
 
-//     const titleElement = document.createElement("p");
-//     titleElement.innerText = "editer";
+const galleryCustom = document.querySelector(".custom_gallery");
 
-//     //Ajout de l'icone supprimé-----------
-//     const iconeElement = document.createElement("div");
-//     iconeElement.classList.add("deletePhoto");
-//     iconeElement.innerHTML =
-//       '<svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M6.6 1.8V0.9C6.6 0.402944 6.19704 0 5.7 0H3.3C2.80294 0 2.4 0.402944 2.4 0.9V1.8H0V2.4H0.6V8.1C0.6 8.59704 1.00294 9 1.5 9H7.5C7.99704 9 8.4 8.59704 8.4 8.1V2.4H9V1.8H6.6ZM3 0.9C3 0.734316 3.13432 0.6 3.3 0.6H5.7C5.86568 0.6 6 0.734316 6 0.9V1.8H3V0.9ZM4.2 4.2V7.2H4.8V4.2H4.2ZM2.4 7.2V5.4H3V7.2H2.4ZM6 5.4V7.2H6.6V5.4H6Z" fill="white"/></svg>';
-
-//     const imageElement = document.createElement("img");
-//     imageElement.src = article.imageUrl;
-
-//     const categoryIdElement = document.createElement("p");
-//     categoryIdElement.innerText = article.categoryId;
-
-//     //Ajout de articleElement dans sectionGallery
-
-//     sectionGallery.appendChild(articleElement);
-
-//     //Ajout de nos balises au DOM
-//     articleElement.appendChild(imageElement);
-//     articleElement.appendChild(titleElement);
-//     articleElement.appendChild(iconeElement);
-
-//     //--------------Suppression photo--------------------------------
-//     iconeElement.addEventListener("click", async (e) => {
-//       e.preventDefault();
-//       e.stopPropagation();
-//       const iconeElement = article.id;
-//       let monToken = localStorage.getItem("token");
-//       console.log(iconeElement);
-//       let response = await fetch(
-//         `http://localhost:5678/api/works/${iconeElement}`,
-//         {
-//           method: "DELETE",
-//           headers: {
-//             accept: "*/*",
-//             Authorization: `Bearer ${monToken}`,
-//           },
-//         }
-//       );
-//       if (response.ok) {
-//         return false;
-//         // if HTTP-status is 200-299
-//         //alert("Photo supprimé avec succes");
-//         // obtenir le corps de réponse (la méthode expliquée ci-dessous)
-//       } else {
-//         alert("Echec de suppression");
-//       }
-//     });
+async function fetchGallery() {
+  const response = await fetch(Url + "works");
+  postsData = await response.json();
+  postsData.map((post) => {
+  const { id, title, imageUrl, category } = post;
+  const gallery = document.createElement("div");
+  gallery.className = "gallery_post";
+  gallery.innerHTML = `<div><div class="gallery_edit"><img class="gallery_image" src="${imageUrl}">
+  <div class="gallery_edit_content">
+  <div class="delete_button"><i class="fa-regular fa-trash-can"></i></div>
+  </div>
+  </div>
+        <p class="post-title">Editer</p>
+        </div>
+  `;
+  galleryCustom.append(gallery);
+});
+// const iconDelete = document.querySelector(".delete_button")
+// //     // Création des balises
+// const article = post;
+// console.log(`${article.id}`)
+// iconDelete.addEventListener("click", async (e) => {
+//   console.log("testClick");
+//   e.preventDefault();
+//   e.stopPropagation();
+//   const iconeElement = article.id;
+//   let monToken = localStorage.getItem("token");
+//   console.log(iconeElement);
+//   let response = await fetch(
+//   `${Url}${iconeElement}`,
+//   {
+//     method: "DELETE",
+//     headers: {
+//       accept: "*/*",
+//       Authorization: `Bearer ${monToken}`,
+//     },
 //   }
-// }
-//     //---------------FIN DE GENERER PHOTO--------------------
+//   );
+//   if (response.ok) {
+//     return false;
+//     // if HTTP-status is 200-299
+//     //alert("Photo supprimé avec succes");
+//     // obtenir le corps de réponse (la méthode expliquée ci-dessous)
+//   } else {
+//     alert("Echec de suppression");
 //   }
+// });
+};
+
+function DeleteWorks(id) {
+let monToken = localStorage.getItem("token");
+fetch(Url + `works/${id}`, {
+        method: "DELETE",
+        headers: {
+          accept: "*/*",
+          Authorization: `Bearer ${monToken}`,
+        },
+}).then(response => {
+  if (response.ok) {
+        alert("Photo supprimé avec succes");
+  } else {
+    alert("Echec de suppression");
+  }
+})
+.catch(error => {
+  console.log(error);
+})
+}
+
+const addWorks = document.querySelector(".add_works");
+
+addWorks.addEventListener("click", addNewWorks)
+
+function addNewWorks() {
+  console.log("addNewWorks");
+}
+
+function PostWorks() {
+fetch(Url + "works", {
+  method: "POST",
+  headers: {
+    "Authorization": `Bearer ${monToken}`,
+    "Content-Type": "application/json",
+  },
+  body: {
+    title: "",
+    imageUrl: "",
+    categoryId: 0,
+  }
+}).then(response => {
+  if (response.ok) {
+    return response.json();
+  } else {
+    return Promise.reject(response.status);
+  }
+})
+.catch(error => {
+  console.log(error);
+})
+}
